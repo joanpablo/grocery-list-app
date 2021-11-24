368e25941604234c7991707af2cd40c1a5ad2b80
JoanPablo <johnpjm85@gmail.com>
2021-11-24 17:57:02 +0200

    add application shell and shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppRootComponent } from './components/app-root/app-root.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppShellComponent } from './components/app-shell/app-shell.component';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
-  declarations: [AppRootComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppRootComponent, AppShellComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppRootComponent],
 })
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppShellComponent } from '../components/app-shell/app-shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'grocery-list', component: AppShellComponent },
+  { path: '', redirectTo: 'grocery-list', pathMatch: 'full' },
+];
